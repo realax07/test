e86d520eba4999f7e55ea9be1b17af1cc52ae3a2
Egor Kotov <ekotov@ozon.ru>
2017-11-09 13:06:29 +0300

    some fixes

--- Ammo tank/Tank.py
+++ Ammo tank/Tank.py
@@ -1,15 +1,5 @@
 # coding: utf8
-#Uri and headers - is <list> type variables
-def get(uri, headers):
-    head, ammo = "", []
-    for h in headers:                                                                                                   # Перебираем массив заголовков, и формируем строку со всеми заголовками и переносами
-        head += h + "\n"
-
-    for u in uri:                                                                                                       # Формируем запрос, который в последствии добавляем в массив ammo
-        ammo.append("GET " + u + " HTTP/1.0" + "\n" + head)
-
-    return ammo                                                                                                         # Возвращаем массив, каждый элемент которого - запрос
-
+import random
 #ar - <list> type
 def get_b(ar):
     a = []
@@ -18,15 +8,29 @@ def get_b(ar):
 
     return a                                                                                                            # Возвращаем массив с посчитанной длиной
 
-#Headers - list type, uribody - dict type, where key is uri, value is body
+#Headers - <list> type
+#Uribody - <dict> type, where key is uri, value is body
 def post(headers, uribody):
-    s = ""
+    st = ""
     for element in headers:                                                                                             # Формируем список заголовков и добавляем его в тело запроса
-        s += "[" + element + "]" + "\n"
+        st += "[" + element + "]" + "\n"
 
     for key in uribody:                                                                                                 # Формируем нагружаемые URL, тело запроса и считаем длину строки в символах.
-        s += str(len(uribody[key])) + " " + key + "\n" + uribody[key] + "\n"
+        st += str(len(uribody[key])) + " " + key + "\n" + uribody[key] + "\n"
 
-    return s                                                                                                            # Возвращаем строку, представляющую собой готовое тело "патрона"
+    return st                                                                                                           # Возвращаем строку, представляющую собой готовое тело "патрона"
 
+#Count - <int>
+#Uri - <list>
+#Headers - <dict> where key is <str> type and value is <list> type
+#Http_ver - <str>
+def get(count, uri, headers, http_ver = "HTTP 1.0"):
+    ammo = []
+    for patron in range(1, count + 1):
+        s = "GET " + uri[random.randint(0, len(uri)-1)] + " " + http_ver
+        for p in headers:
+            s += "\n" + p + ": " + headers[p][random.randint(0, len(headers[p])-1)]
+        ammo.append(s)
+
+    return ammo
 
